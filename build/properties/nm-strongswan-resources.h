ba9b464e99057251a2a033107cbfa33b13088c4e
xodefender <kir.zhilnikov37@internet.ru>
2025-04-21 15:35:02 +0300

    init

---
+++ build/properties/nm-strongswan-resources.h
@@ -0,0 +1,7 @@
+#ifndef __RESOURCE_nm_strongswan_dialog_H__
+#define __RESOURCE_nm_strongswan_dialog_H__
+
+#include <gio/gio.h>
+
+G_GNUC_INTERNAL GResource *nm_strongswan_dialog_get_resource (void);
+#endif
